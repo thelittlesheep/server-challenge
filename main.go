ba06f210d00f099a9238859ba6bd415d3e594a04
thelittlesheep <lshuangktw@gmail.com>
2023-03-10 17:38:09 +0800

    config: using dotenv to set server port

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
+func main() {
+	var myEnv map[string]string
+	myEnv, _ = godotenv.Read()
+
+	router := gin.Default()
+
+	router.Run(":" + myEnv["PORT"])
 }
