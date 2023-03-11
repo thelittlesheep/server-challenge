865ebee0637f994912ab79323a81d06ba34f146a
thelittlesheep <lshuangktw@gmail.com>
2023-03-11 14:46:15 +0800

    config: use env file to control ginMode

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ func main() {
 	var myEnv map[string]string
 	myEnv, _ = godotenv.Read()
 
+	var ginMode string
+	if myEnv["ENV"] != "production" {
+		ginMode = gin.DebugMode
+	} else {
+		ginMode = gin.ReleaseMode
+	}
+
+	gin.SetMode(ginMode)
+
 	router := gin.Default()
 
 	router.Run(":" + myEnv["PORT"])
