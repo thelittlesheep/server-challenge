54f963126b2352a46ca3aa1618bf3da445c0e753
thelittlesheep <lshuangktw@gmail.com>
2023-03-12 19:36:38 +0800

    db: create main database connection

---
+++ models/index.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// DB create a new instance of GORM
+func DB() (*gorm.DB, error) {
+	var myEnv map[string]string
+	myEnv, _ = godotenv.Read()
+	// read environment variables
+	dsn := myEnv["DATABASE_DRIVER"] + "://" +
+		myEnv["DATABASE_USERNAME"] + ":" +
+		myEnv["DATABASE_PASSWORD"] + "@" +
+		myEnv["DATABASE_HOST"] + ":" +
+		myEnv["DATABASE_PORT"] + "/" +
+		myEnv["DATABASE_NAME"]
+	// open a new database connection
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	// return the GORM instance
+	return db, nil
+}
