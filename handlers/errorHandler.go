485f7f2364c239f440ecee8e8a37e9de477b12c9
thelittlesheep <lshuangktw@gmail.com>
2023-03-14 13:13:11 +0800

    feat: add error view

--- handlers/errorHandler.go
+++ handlers/errorHandler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"regexp"
 )
 
 func ErrorHandler(ctx *gin.Context) {
@@ -16,20 +17,30 @@ func ErrorHandler(ctx *gin.Context) {
 
 		statusCode := ctx.Writer.Status()
 
-		switch statusCode {
-		case 401:
-			ctx.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Unauthorized",
+		matchApiUrl, _ := regexp.MatchString("^/api/.*", ctx.Request.URL.Path)
+
+		if !matchApiUrl && statusCode != 200 {
+			ctx.HTML(http.StatusOK, "error.html", gin.H{
+				"code":    statusCode,
+				"message": ctx.Errors.Last(),
 			})
-		case 403:
-			ctx.JSON(http.StatusForbidden, gin.H{
-				"message": "Forbidden",
-			})
-		case 404:
-			ctx.JSON(http.StatusNotFound, gin.H{
-				"message": "Not Found",
-			})
+		} else {
+			switch statusCode {
+			case 401:
+				ctx.JSON(http.StatusUnauthorized, gin.H{
+					"message": "Unauthorized",
+				})
+			case 403:
+				ctx.JSON(http.StatusForbidden, gin.H{
+					"message": "Forbidden",
+				})
+			case 404:
+				ctx.JSON(http.StatusNotFound, gin.H{
+					"message": "Not Found",
+				})
+			}
 		}
+
 		ctx.Abort()
 	}()
 
